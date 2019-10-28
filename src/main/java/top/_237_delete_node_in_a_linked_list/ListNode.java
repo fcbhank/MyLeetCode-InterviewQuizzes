63c5ebea1f7ab33e66274733211ad75453a46de3
fcbhank <hankhu4work@163.com>
2019-10-29 00:51:55 +0800

    237.删除列表中的节点

---
+++ src/main/java/top/_237_delete_node_in_a_linked_list/ListNode.java
@@ -0,0 +1,14 @@
+package top._237_delete_node_in_a_linked_list;
+
+/**
+ * Created by hank on 2019/10/29
+ */
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/top/_237_delete_node_in_a_linked_list/Solution.java
@@ -0,0 +1,22 @@
+package top._237_delete_node_in_a_linked_list;
+
+/**
+ * Created by hank on 2019/10/29
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        // 直接往后移值，由于删除元素不是尾部元素，所以当碰到 next 指向 null时，即可让 pre 指针指向 null
+        // 这样就减去了一个元素
+        ListNode pre = null;
+        while (node != null && node.next != null) {
+            pre = node;
+            ListNode next = node.next;
+            node.val = next.val;
+            node = next;
+        }
+        if (node.next == null) {
+            pre.next = null;
+        }
+
+    }
+}
