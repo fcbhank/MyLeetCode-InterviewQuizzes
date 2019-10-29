9630ec7e0077950d91fd9041af0ffde320e1e248
fcbhank <hankhu4work@163.com>
2019-10-29 21:06:57 +0800

    292未完成

---
+++ src/main/java/tencent50/_292_nim_game/Solution.java
@@ -0,0 +1,14 @@
+package tencent50._292_nim_game;
+
+
+class Solution {
+    public boolean canWinNim(int n) {
+        return judge(n, 0, 1);
+    }
+
+    private boolean judge(int left, int take, int numbers) {
+        return false;
+
+    }
+
+}
