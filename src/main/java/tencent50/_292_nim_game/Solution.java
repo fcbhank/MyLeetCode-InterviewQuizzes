54cfbaefc13ae397b2aeba2021f3a5de6b50e5b3
fcbhank <hankhu4work@163.com>
2019-10-31 00:22:59 +0800

    292. Nim 游戏

--- src/main/java/tencent50/_292_nim_game/Solution.java
+++ src/main/java/tencent50/_292_nim_game/Solution.java
@@ -3,12 +3,7 @@ package tencent50._292_nim_game;
 
 class Solution {
     public boolean canWinNim(int n) {
-        return judge(n, 0, 1);
+        // 数学归纳法，要想赢不能是 4 的倍数
+        return n % 4 != 0;
     }
-
-    private boolean judge(int left, int take, int numbers) {
-        return false;
-
-    }
-
 }
