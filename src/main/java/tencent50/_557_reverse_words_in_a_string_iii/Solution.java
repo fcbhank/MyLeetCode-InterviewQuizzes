fa2f1571352cd63949c52a6db8733cb8a6fa5ac2
fcbhank <hankhu4work@163.com>
2019-10-31 13:35:12 +0800

    557. 反转字符串中的单词 III

---
+++ src/main/java/tencent50/_557_reverse_words_in_a_string_iii/Solution.java
@@ -0,0 +1,31 @@
+package tencent50._557_reverse_words_in_a_string_iii;
+
+import com.sun.tools.javac.util.StringUtils;
+
+/**
+ * @author blind_panther
+ * @date 2019/10/31 下午1:08
+ */
+public class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        String buffer = "";
+        for (String word : words) {
+            buffer += reverseAWord(word) + " ";
+        }
+        int pos = buffer.length() - 1;
+        String finalStr = buffer.substring(0, pos);
+        return finalStr;
+    }
+
+    public String reverseAWord(String word) {
+        int len = word.length();
+        char[] chars = word.toCharArray();
+        for (int i = 0, j = len - 1; i < j; i++, j--) {
+            char temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+        }
+        return new String(chars);
+    }
+}
---
+++ src/test/java/tencent50/_557_reverse_words_in_a_string_iii/SolutionTest.java
@@ -0,0 +1,14 @@
+package tencent50._557_reverse_words_in_a_string_iii;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author blind_panther
+ * @date 2019/10/31 下午1:19
+ */
+public class SolutionTest {
+    @Test
+    public void test(){
+        System.out.println(new Solution().reverseWords("Let's take LeetCode contest"));
+    }
+}
