8db5279a90a725da53defeeed973fe18a90aeabb
fcbhank <hankhu4work@163.com>
2019-11-02 00:51:06 +0800

    235. 二叉搜索树的最近公共祖先

--- src/main/java/tencent50/easy/_253_lowest_common_ancestor_of_a_binary_search_tree/Solution.java
+++ src/main/java/tencent50/easy/_253_lowest_common_ancestor_of_a_binary_search_tree/Solution.java
@@ -7,7 +7,33 @@ package tencent50.easy._253_lowest_common_ancestor_of_a_binary_search_tree;
 public class Solution {
     // 递归法
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return findPorQ(root, p, q);
+    }
 
+    /**
+     * @param node 当前根结点
+     * @param p
+     * @param q
+     * @return node 下如果能找到 p 就返回 p，能找到 q 就返回 q，如果都能找到就返回 node
+     */
+    private TreeNode findPorQ(TreeNode node, TreeNode p, TreeNode q) {
+        if (node == null) {
+            return null;
+        }
+        if (node == p || node == q) {
+            // 说明此时 p 或 q 为对方的父辈结点
+            return node;
+        }
+
+        TreeNode left = findPorQ(node.left, p, q);
+        TreeNode right = findPorQ(node.right, p, q);
+        if (left != null && right != null) {
+            return node;
+        } else if (left == null && right != null) {
+            return right;
+        } else if (left != null) {
+            return left;
+        }
         return null;
     }
 }
